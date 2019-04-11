6f73c40c44057779fc648edca3caac410ec399b8
KarolisA <k.aidukonis@gmail.com>
2019-04-11 18:17:52 +0300

    PasswordEncryption
    Slaptazodziu encryptinimas su SpringBoot ir BCrypt

---
+++ SpringSecurityJdbcPassEncryption-master/src/main/java/passwords/AuthenticationProviderConfig.java
@@ -0,0 +1,28 @@
+package passwords;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+
+@Configuration
+public class AuthenticationProviderConfig {
+	@Bean(name = "dataSource")
+	public DriverManagerDataSource dataSource() {
+	    DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+	    driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+	    driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/draudimas");
+	    driverManagerDataSource.setUsername("root");
+	    driverManagerDataSource.setPassword("root");
+	    return driverManagerDataSource;
+	}
+    
+    @Bean(name="userDetailsService")
+    public UserDetailsService userDetailsService(){
+    	JdbcDaoImpl jdbcImpl = new JdbcDaoImpl();
+    	jdbcImpl.setDataSource(dataSource());
+    	jdbcImpl.setUsersByUsernameQuery("select username,password, enabled from users where username=?");
+    	return jdbcImpl;
+    }
+}
