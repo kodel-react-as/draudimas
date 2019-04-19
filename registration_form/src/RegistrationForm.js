817bc23100cc1960e9f1f4b9c6627669f1d0e874
Lukas_Sapronas <sapronas.lukas@gmail.com>
2019-04-19 11:22:55 +0300

    Update RegistrationForm.js

--- registration_form/src/RegistrationForm.js
+++ registration_form/src/RegistrationForm.js
@@ -3,67 +3,200 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 class RegistrationForm extends React.Component {
 
+    state = {
+        firstName: '',
+        lastName: '', 
+        idNumber: '',
+        country: '',
+        city: '',
+        adress: '',
+        password: ''
+    }
+
+    handleFirstNameChange = event => {
+        this.setState({firstName: event.target.value});
+    }
+
+    handleLastNameChange = event => {
+        this.setState({lastName: event.target.value});
+    }
+
+    handleIdNumberChange = event => {
+        this.setState({idNumber: event.target.value});
+    }
+
+    handleCountryChange = event => {
+        this.setState({country: event.target.value});
+    }
+
+    handleCityChange = event => {
+        this.setState({city: event.target.value});
+    }
+
+    handleAdressChange = event => {
+        this.setState({adress: event.target.value});
+    }
+
+    handlePasswordChange = event => {
+        this.setState({password: event.target.value});
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const firstName = this.state.firstName;
+        const lastName = this.state.lastName;
+        const idNumber = this.state.idNumber;
+        const country = this.state.country;
+        const city = this.state.city;
+        const adress = this.state.adress;
+        const password = this.state.password;
+
+        console.log(firstName + " " + lastName + " " + idNumber + " " + country + " " + city + " " + adress + " " + password);
+    }
+
     render() {
         return(
             <div className="container">
-                <form>
+                <form onSubmit={this.handleSubmit}>
+
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-12">
                             <div className="form-group">
                                 <label>Firstname:</label>
-                                <input type="text" className="form-control" name="FirstName" placeholder="Your first name" required />
-                            </div>
-                        </div>
-                        <div className="col-lg-6">
-                            <div className="form-group">
-                                <label>Lastname:</label>
-                                <input type="text" className="form-control" name="LastName" placeholder="Your last name" required />
+
+                                <input 
+                                    type="text" 
+                                    className="form-control"
+                                    placeholder="Your first name"
+                                    onChange={this.handleFirstNameChange}
+                                    value={this.state.firstName}
+                                    required 
+                                />
+
                             </div>
                         </div>
                     </div>
+
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-12">
+                                <div className="form-group">
+                                    <label>Lastname:</label>
+
+                                    <input
+                                        type="text" 
+                                        className="form-control" 
+                                        placeholder="Your last name" 
+                                        onChange={this.handleLastNameChange}
+                                        value={this.state.lastName}
+                                        required
+                                    />
+
+                                </div>
+                        </div>
+                    </div>
+                        
+                    <div className="row">
+                        <div className="col-lg-12">
                             <div className="form-group">
                                 <label>ID number:</label>
-                                <input type="text" className="form-control" name="personIdNumber" placeholder="Your ID number" pattern="^[0-9]*$" required />
+
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Your ID number" 
+                                    pattern="^[0-9]*$"
+                                    onChange={this.handleIdNumberChange}
+                                    value={this.state.idNumber}
+                                    required
+                                />
+
                             </div>
                         </div>
-                        <div className="col-lg-6">
+                    </div>
+
+                    <div className="row">
+                        <div className="col-lg-12">
                             <div className="form-group">
                                 <label>Country</label>
-                                <input type="text" className="form-control" name="country" placeholder="Your country" required />
+
+                                <input
+                                    type="text" 
+                                    className="form-control"
+                                    placeholder="Your country"
+                                    onChange={this.handleCountryChange}
+                                    value={this.state.country}
+                                    required
+                                />
+
                             </div>
                         </div>
                     </div>
+                        
+                    
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-12">
                             <div className="form-group">
                                 <label>City</label>
-                                <input type="text" className="form-control" name="city" placeholder="Your city" required />
+
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Your city"
+                                    onChange={this.handleCityChange}
+                                    value={this.state.city}
+                                    required
+                                />
                             </div>
                         </div>
-                        <div className="col-lg-6">
+                    </div>
+
+                    <div className="row">
+                        <div className="col-lg-12">
                             <div className="form-group">
                                 <label>Adress</label>
-                                <input type="text" className="form-control" name="adress" placeholder="Your adress" required />
+
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Your adress"
+                                    onChange={this.handleAdressChange}
+                                    value={this.state.value}
+                                    required
+                                />
+
                             </div>
                         </div>
                     </div>
+
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-12">
                             <div className="form-group">
                                 <label>Password:</label>
-                                <input type="password" className="form-control" name="password" placeholder="Your password" required />
+
+                                <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Your password"
+                                onChange={this.handlePasswordChange}
+                                value={this.state.password}
+                                required
+                                />
+
                             </div>
                         </div>
                     </div>
+
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-12">
                             <div className="form-group">
+
                                 <input type="submit" className="btn btn-primary" />
+                                
                             </div>
                         </div>
                     </div>
+
                 </form>
             </div>
         )
